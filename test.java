5c7e2dda7b4ae1c306a678ea94d4d4febb786b05
jayant <kumarjayant936@gmail.com>
2024-11-28 08:30:23 +0530

    test java file

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test{
+	public static void main (String [] args){
+	System.out.println ("Hello ");
+	    System.out.println ("Hello to Raj gd meng ljhehf");
+	     System.out.println ("Hello to all anf bey good mornijgn");
+	     System.out.println ("hello mam");
+	 }
+       }
+
